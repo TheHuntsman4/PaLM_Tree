ea6d8345622f2b1468dd70f860526c2c3bd3b19b
TheHuntsman4 <anikethvij464@gmail.com>
2023-08-07 12:26:29 +0530

    Feat Adds an example for abstract class

--- Prototype.java
+++ Prototype.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Prototype {
+public abstract class Prototype {
     static class Request {
         public String request = "tell me about yourself";
         public int numberOfTokens;
